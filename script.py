efec6c2844848b6249840417e17b9cca22040831
DanVan-rgb <valeriadianaglz@gmail.com>
2026-06-03 20:41:23 -0600

    Se agrega script.py que imprime nombre y comandos favoritos

---
+++ script.py
@@ -0,0 +1,7 @@
+nombre = "Diana Valeria González Palomino"
+comandos_favoritos = ["git status", "git commit", "git push"]
+
+print(f"Nombre del estudiante: {nombre}")
+print("Mis 3 comandos de Git favoritos son:")
+for comando in comandos_favoritos:
+    print(f"- {comando}")
